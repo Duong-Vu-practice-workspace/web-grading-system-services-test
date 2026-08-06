d059f84de52724a69013d0d7eaaf7582c1fd38d4
Duong Vu Cong Tuan <vucongtuanduong2409@gmail.com>
2026-08-06 17:22:14 +0700

    test feat branch

--- result-service/src/main/java/vn/edu/ptit/web_grading_system/result_service/ResultServiceApplication.java
+++ result-service/src/main/java/vn/edu/ptit/web_grading_system/result_service/ResultServiceApplication.java
@@ -20,7 +20,7 @@ public class ResultServiceApplication {
 
 	@Bean
 	public CommandLineRunner startupInfo() {
-		return args -> log.info("Result Service started successfully");
+		return args -> log.info("Result Service started successfully!!!");
 	}
 
 }
